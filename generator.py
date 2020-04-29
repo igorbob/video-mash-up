a37d4bb37b6789c42943cf51697ef4dcf9537b6b
Igor Bobeldijk <igor.bobeldijk@gmail.com>
2020-04-29 16:39:46 +0200

    major update

--- generator.py
+++ generator.py
@@ -1,7 +1,4 @@
-
-
-SECONDS_PER_CLIP = 2
-
+from collections import OrderedDict
 from random import shuffle
 from math import floor
 import os.path
@@ -9,104 +6,150 @@ from glob import glob
 from pathlib import Path
 import math
 import pandas as pd
+import json
 from cv2 import cv2
 from SSIM_PIL import compare_ssim
 from PIL import Image
 from tqdm import tqdm
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-def unpack_frames():
-    project_dir = Path(__file__).parent
-    video_paths = glob(str(project_dir / 'videos' / '*.mp4'))
-    for video in video_paths:
-        video_id_mp4 = os.path.basename(video)
-        video_id = os.path.splitext(video_id_mp4)[0]
-        video_frames_folder = project_dir / 'frames' / video_id
 
-        if video_frames_folder.exists():
-            print(f"{video_id} already extracted")
-        else:
-            video_frames_folder.mkdir()
-            cap = cv2.VideoCapture(video)
-            frame_rate = cap.get(5)
-            while(cap.isOpened()):
-                frameId = cap.get(1) #current frame number
-                retrival, frame = cap.read()
-                if not retrival:
-                    break
-                if (frameId % math.floor(frame_rate*4) == 0):
-                    filename = str(video_frames_folder / f'{int(frameId)}.jpg')
-                    print(filename)
+def root_dir():
+    return Path(__file__).parent
+
+
+def unpack_frames(new_size):
+    video_paths = glob(str(root_dir() / "videos" / "*.mp4"))
+    # frame_rates = json.load
+
+    # with open('frame_rates.txt') as json_file:
+    #     frame_rates = json.load(json_file)
+
+    frame_rates = {}
+
+    with tqdm(total=len(video_paths)) as pbar:
+        for video in video_paths:
+            video_id_mp4 = os.path.basename(video)
+            video_id = os.path.splitext(video_id_mp4)[0]
+            video_frames_folder = root_dir() / "frames" / video_id
+
+            if not video_frames_folder.exists():
+                video_frames_folder.mkdir()
+                cap = cv2.VideoCapture(video)
+
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                frame_rates[video_id] = fps
+                while cap.isOpened():
+                    frameId = cap.get(1)  # current frame number
+                    retrival, frame = cap.read()
+                    if not retrival:
+                        break
+                    if new_size:
+                        frame = cv2.resize(frame, new_size)
+                    filename = str(video_frames_folder / f"{int(frameId)}.png")
                     cv2.imwrite(filename, frame)
-            cap.release()
-            print ("Done!")
-
-def resize_all():
-    project_dir = Path(__file__).parent
-    video_filenames = [os.path.basename(video) for video in glob(str(project_dir / 'videos' / '*.mp4'))]
-    video_ids = [os.path.splitext(video_filename)[0] for video_filename in video_filenames]
-
-    with tqdm(total=len(video_ids)) as pbar:
-        for video_id in video_ids:
-            frames = glob(str(project_dir / 'frames' / video_id / '*.jpg'))
-            for frame in frames:
-                image = Image.open(frame).resize((128,128))
-                image.save(frame)
-            pbar.set_description(f"Processing {video_id}")
+                cap.release()
+            pbar.set_description(f"Unpacking {video_id}")
             pbar.update(1)
-    
-def make_connections():
-    project_dir = Path(__file__).parent
-    video_filenames = [os.path.basename(video) for video in glob(str(project_dir / 'videos' / '*.mp4'))]
-    video_ids = [os.path.splitext(video_filename)[0] for video_filename in video_filenames]
+    return frame_rates
+
+
+def get_framerates():
+    video_paths = glob(str(root_dir() / "videos" / "*.mp4"))
+
+    frame_rates = {}
+    with tqdm(total=len(video_paths)) as pbar:
+        for video in video_paths:
+            video_id_mp4 = os.path.basename(video)
+            video_id = os.path.splitext(video_id_mp4)[0]
+            cap = cv2.VideoCapture(video)
+            frame_rates[video_id] = cap.get(cv2.CAP_PROP_FPS)
+            pbar.update(1)
+    return frame_rates
+
+
+def make_connections(frame_rates, num_connections, seconds_per_clip):
+    video_filenames = [
+        os.path.basename(video) for video in glob(str(root_dir() / "videos" / "*.mp4"))
+    ]
+    video_ids = [
+        os.path.splitext(video_filename)[0] for video_filename in video_filenames
+    ]
     shuffle(video_ids)
 
-    connections = {}
+    current_video_id = video_ids.pop()
+    starting_frame = 1
+    fps = frame_rates[current_video_id]
 
-    for i in range(20):
-        video_id = video_ids.pop()
-        frames = glob(str(project_dir / 'frames' / video_id / '*.jpg'))
-
-        two_thirds = floor(len(frames) * 0.6)
-        frame_two_thirds = frames[two_thirds]
-
-        image_1 = Image.open(frame_two_thirds)
+    connections = OrderedDict()
+    connections[current_video_id] = starting_frame
+    for _ in range(num_connections):
+        frames = glob(str(root_dir() / "frames" / current_video_id / "*.jpg"))
 
         max_ssi = 0.0
-        max_meta = ('','')
-        with tqdm(total=len(video_ids)) as pbar:
-            for other_video_id in video_ids:
-                other_frames = glob(str(project_dir / 'frames' / other_video_id / '*.jpg'))
-                new_ssi, _ = get_max_ssi(image_1, other_frames, 7)
-                if new_ssi > max_ssi:
-                    new_ssi, new_frame = get_max_ssi(image_1, other_frames)
-                    max_ssi = new_ssi
-                    matching_video_id = other_video_id
-                    matching_frame = new_frame
-                pbar.set_description(f"Comparing {video_id} to {other_video_id}")
-                pbar.update(1)
 
-        connections[video_id] = {
-            "frame_nr": two_thirds,
-            "max_ssi": max_ssi,
-            "matching_video_id": matching_video_id,
-            "matching_frame": matching_frame,
-        }
-        print(connections)
+        with Image.open(frames[starting_frame + (seconds_per_clip * fps)]) as image_1:
+            with tqdm(total=len(video_ids)) as pbar:
+                for other_video_id in video_ids:
+                    other_frames = glob(
+                        str(root_dir() / "frames" / other_video_id / "*.jpg")
+                    )
+                    new_ssi, _ = get_max_ssi(
+                        image_1, other_frames[: -(seconds_per_clip * fps)], 5
+                    )
+                    if new_ssi > max_ssi:
+                        new_ssi, matching_frame = get_max_ssi(
+                            image_1, other_frames[: -(seconds_per_clip * fps)]
+                        )
+                        max_ssi = new_ssi
+                        matching_video_id = other_video_id
+                    pbar.set_description(
+                        f"Comparing {current_video_id} to {other_video_id}"
+                    )
+                    pbar.update(1)
+
+        connections[matching_video_id] = matching_frame
+
+        current_video_id = matching_video_id
+        video_ids.remove(current_video_id)
+        starting_frame = matching_frame
+        fps = frame_rates[current_video_id]
+
+    return connections
 
 
 def get_max_ssi(image_1, other_frames, grain=1):
     max_ssi = 0.0
-    max_frame = ""
+
     for other_frame in other_frames[::grain]:
-        image_2 = Image.open(other_frame)
-        ssi = compare_ssim(image_1, image_2)
-        if ssi > max_ssi:
-            max_ssi = ssi
-            max_frame = other_frame
-    return max_ssi, max_frame
+        with Image.open(other_frame) as image_2:
+            ssi = compare_ssim(image_1, image_2)
+            if ssi > max_ssi:
+                max_ssi = ssi
+                matching_frame = other_frame
+    return max_ssi, matching_frame
 
 
-if __name__ == '__main__':
-    #unpack_frames()
-    #resize_all()
-    make_connections()
+def stitch_video_together(frame_rates, connections, seconds_per_clip):
+    clips = []
+    for video_id, starting_frame in connections.items():
+        start = starting_frame / frame_rates[video_id]
+        video_path = Path(__file__).parent / "videos" / f"{video_id}.mp4"
+        clip = (
+            VideoFileClip(str(video_path), audio=False, target_resolution=(480, 720))
+            .subclip(start, start + seconds_per_clip)
+            .set_fps(25)
+        )
+        clips.append(clip)
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile("my_concatenation.mp4")
+
+
+if __name__ == "__main__":
+    seconds_per_clip = 2
+    num_connections = 30
+
+    # frame_rates = unpack_frames(new_size=(64, 64))
+
+    connections = make_connections(frame_rates, num_connections, seconds_per_clip)
+    stitch_video_together(frame_rates, connections, seconds_per_clip)
