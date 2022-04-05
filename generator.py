ec99f33db56f6805d0acc98fd82f3af1a42e62bb
Igor Bobeldijk <igor.bobeldijk@gmail.com>
2022-04-05 20:24:08 +0200

    new_version

--- generator.py
+++ generator.py
@@ -1,23 +1,23 @@
-from collections import OrderedDict
 from random import shuffle
-
-import time
-import os.path
 from glob import glob
 from pathlib import Path
 
-import json
-from cv2 import cv2
 from SSIM_PIL import compare_ssim
 from PIL import Image
 from tqdm import tqdm
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import moviepy.video.fx.all as vfx
 
-from video import Video
+from pympler import muppy, summary
+
+from my_video import Video
+
+import tracemalloc
+
+tracemalloc.start(10)
 
 SECONDS_PER_CLIP = 3
-NUM_CONNECTIONS = 7
+NUM_CONNECTIONS = 5
 STARTING_FRAME = 45
 
 def root_dir():
@@ -27,6 +27,8 @@ def root_dir():
 def find_connections(videos, num_connections, seconds_per_clip):
 
     shuffle(videos)
+
+    videos = videos[:2000]
 
     current_video = videos.pop()
     current_video.set_in_and_out_frame(STARTING_FRAME, SECONDS_PER_CLIP)
@@ -65,6 +67,7 @@ def find_connections(videos, num_connections, seconds_per_clip):
 
 def get_max_ssi(image_1, video, skip=1):
     max_ssi = -1.0
+    matching_frame = 0
 
     for frame in range(0, video.frame_count, skip):
         with Image.open(video.get_frame_path(frame)) as image_2:
@@ -77,55 +80,76 @@ def get_max_ssi(image_1, video, skip=1):
 def stitch_video_together(connections, seconds_per_clip):
     clips = []
     for video in connections:
+        start = video.out_frame / video.fps
+
+        clip = (
+                VideoFileClip(str(video.path), audio=False, target_resolution=(480, 720))
+                .subclip(start, start + seconds_per_clip)
+            )
+
         if video.is_reversed:
-            start = video.out_frame / video.fps
-            print(video.fps)
-            clip = (
-                VideoFileClip(str(video.path), audio=False, target_resolution=(480, 720))
-                .subclip(start, start + seconds_per_clip)
-                .fx(vfx.time_mirror)
-                .set_fps(25)
-            )
-        else:
-            start = video.in_frame / video.fps
-            clip = (
-                VideoFileClip(str(video.path), audio=False, target_resolution=(480, 720))
-                .subclip(start, start + seconds_per_clip)
-                .set_fps(25)
-            )
+            clip = clip.fx(vfx.time_mirror)
+
+        clip = clip.set_fps(25)
+
         clips.append(clip)
     final_clip = concatenate_videoclips(clips)
-    final_clip.write_videofile(f"test_{time.strftime(r'%H:%M')}.mp4")
+    final_clip.write_videofile(f"result_{connections[-1].vid_id}_{connections[-1].out_frame}.mp4")
 
 
-if __name__ == "__main__":
-
+def find_connections_and_stitch():
     videos = [Video(path) for path in glob(str(root_dir() / "videos" / "*.mp4"))]
     connections = find_connections(videos, NUM_CONNECTIONS, SECONDS_PER_CLIP)
 
-    # conn = {"_o76YUyNl0s":45,
-    #         "ooIONZJ8VWA":1950,
-    #         "DP-JWjAIYK8":3911,
-    #         "cO5snUzoVv0":3055,
-    #         "HP7iIKcMLvo":2234,
-    #         "8kFEZ3q1Uvo":3531,
-    #         "5qxWdN5kFOI":42,
-    #         "Nk33m_oJuPg":1436,
-    #         "yclx0NnvdBU":1625,
-    #         "MapXib82Mtc":2509,}
-    # connections = []
-    # for key, value in conn.items():
-    #     video = Video(f"videos/{key}.mp4")
-    #     video.set_in_and_out_frame(value, SECONDS_PER_CLIP)
-    #     print(video.is_reversed)
-    #     connections.append(video)
-
-    for connection in connections:
-        print(connection.vid_id)
-        print(connection.in_frame)
+#     conn = {
+# "jnddxMFLGJE":45,
+# "Pfjr0JeTJPs":11215,
+# "FruXl5cdQ20":585,
+# "_LZ4m0tMmAs":710,
+# "b-y6zZYVO68":520,
+# "Kd3BpQ3ZoRU":4157,
+#     }
+#     connections = []
+#     for key, value in conn.items():
+#         video = Video(f"videos/{key}.mp4")
+#         video.set_in_and_out_frame(value, SECONDS_PER_CLIP)
+#         print(video.is_reversed)
+#         connections.append(video)
 
     stitch_video_together(connections, SECONDS_PER_CLIP)
 
+if __name__ == "__main__":
+
+    try:
+        find_connections_and_stitch()
+    except:
+        all_objects = muppy.get_objects()
+        sum1 = summary.summarize(all_objects)
+        summary.print_(sum1)
+        # snapshot = tracemalloc.take_snapshot()
+        # print(snapshot)
 
 
+
+
+# conn = {"_o76YUyNl0s":45,
+#         "ooIONZJ8VWA":1950,
+#         "DP-JWjAIYK8":3911,
+#         "cO5snUzoVv0":3055,
+#         "HP7iIKcMLvo":2234,
+#         "8kFEZ3q1Uvo":3531,
+#         "5qxWdN5kFOI":42,
+#         "Nk33m_oJuPg":1436,
+#         "yclx0NnvdBU":1625,
+#         "MapXib82Mtc":2509,}
+# connections = []
+# for key, value in conn.items():
+#     video = Video(f"videos/{key}.mp4")
+#     video.set_in_and_out_frame(value, SECONDS_PER_CLIP)
+#     print(video.is_reversed)
+#     connections.append(video)
+
+# for connection in connections:
+#     print(connection.vid_id)
+#     print(connection.in_frame)
 
--- harvester.py
+++ harvester.py
@@ -6,10 +6,11 @@ from pytube import YouTube
 def download_into_folder(video_id, dir):
     try:
         yt = YouTube(f"https://youtu.be/{video_id}")
-        stream = yt.streams.get_by_resolution("360p")
-        if stream.filesize_approx > 0.2e6 and stream.filesize_approx < 9e6:
-            print(f"\n downloading {video_id}")
-            stream.download(dir, filename=video_id)``
+        
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        if stream.filesize_approx > 0.2e6 and stream.filesize_approx < 30e6:
+            print(f"\n downloading {video_id} (file size: {stream.filesize_approx/1e6}MB)")
+            stream.download(dir, filename=f"{video_id}.mp4")
         else:
             print(f"skipping {video_id}, file: {stream.filesize_approx/1e6}MB")
         del yt
@@ -34,7 +35,7 @@ def get_video_ids(query):
 if __name__ == "__main__":
     videos_dir = Path(__file__).parent / "videos"
 
-    queries = [f"MOV{n:05d}" for n in range(460, 512)]  # 1 - 46, 60 -99
+    queries = [f"MOV{n:05d}" for n in range(4715, 10000)]
 
     for query in queries:
         video_ids = get_video_ids(query)
